5495452d4429e485fc24a71af61788866a25f7bf
Nuria <soriano.nuria@gmail.com>
2016-08-01 12:06:03 +0200

    constructor function, cleanup

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var beatles = require('./data.js');
 var dateFormat = require('dateformat');
 
 beatles.members.forEach(function(elem,index){
-	var age = beatles.getAge(beatles.members[index]);
-	var status = 'is ';
+	var age = beatles.getAge(beatles.members[index].birth);
 	var birth = dateFormat(elem.birth, 'ddd mmm dd yyyy');
-	if(elem.dead === true ){
+	var status = 'is ';
+	if(elem.isDead === true ){
 		var status = 'would be ';
 	}
 	console.log( elem.name + ' was born on ' + birth + ' and ' + status + age );
--- data.js
+++ data.js
@@ -1,34 +1,23 @@
 var Beatles = function () {};
 
+function Beatle(name, birthDate, dead){
+	this.name = name;
+	this.birth = new Date(birthDate);
+	this.isDead = dead;
+}
+
 Beatles.prototype.members = [
-	{
-		name: 'John',
-		birth: new Date('October 13, 1940'),
-		dead: true
-	},
-	{
-		name: 'Paul',
-		birth: new Date('June 18, 1942'),
-		dead: false
-	},
-	{
-		name: 'George',
-		birth: new Date('February 25, 1943'),
-		dead: true
-	},
-	{
-		name: 'Ringo',
-		birth: new Date('July 7, 1940'),
-		dead: false
-	},
+	new Beatle('John', 'October 9, 1940', true),
+	new Beatle('Paul', 'June 18, 1942', false),
+	new Beatle('George', 'February 25, 1943', true),
+	new Beatle('Ringo', 'July 7, 1940', false),
 ];
 
-Beatles.prototype.getAge = function (member) {
-	var birth = member.birth;
+Beatles.prototype.getAge = function (birthDate) {
 	var now = new Date();
-	var age = now.getFullYear() - birth.getFullYear();
-	if ((birth.getMonth() > now.getMonth()) || 
-		(birth.getMonth() === now.getMonth() && birth.getDate() > now.getDate()) ){
+	var age = now.getFullYear() - birthDate.getFullYear();
+	if ((birthDate.getMonth() > now.getMonth()) || 
+		(birthDate.getMonth() === now.getMonth() && birthDate.getDate() > now.getDate()) ){
 		age--;
 	}
 	return age;
