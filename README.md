fb69d703ad0a1cc8a81926567b0de23bb7565ed7
Nuria <soriano.nuria@gmail.com>
2016-08-01 11:32:39 +0200

    add readme and dateformat

---
+++ README.md
@@ -0,0 +1,2 @@
+#node-beatles
+Learning node
--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var beatles = require('./data.js');
+var dateFormat = require('dateformat');
 
 beatles.members.forEach(function(elem,index){
 	var age = beatles.getAge(beatles.members[index]);
-	var status = ' is ';
+	var status = 'is ';
+	var birth = dateFormat(elem.birth, 'ddd mmm dd yyyy');
 	if(elem.dead === true ){
-		var status = ' would be ';
+		var status = 'would be ';
 	}
-	console.log( elem.name + status + age );
+	console.log( elem.name + ' was born on ' + birth + ' and ' + status + age );
 })
